d4717269cc4a29e95065d51ef4605d6898561f72
Guimonteirol <gui.gjacobina@gmail.com>
2021-12-26 04:20:02 -0300

    Criação das rotas da página do app

--- src/moduloControlerLogin.js
+++ src/moduloControlerLogin.js
@@ -10,22 +10,33 @@ class ControlerUserLogin{
             this.status = true;
         }
 
-        loginInfo(user, senha){
+        checkOutLogin(){
+            this.status = false;
+        }
+
+        loginInfo(user, senha){ 
             var infoCheck = userLogin.find(userLogin => userLogin.username === user ) //retorna um vetor com os objetos - procurar dentro dos dados
         // console.log(infoCheck) //aqui vai mostrar o vetro de dados q foi encontrtado pelo (.find)
             if( user === null || senha === null || user === "" || senha === ""){
-                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "camposVazios1")
+                this.checkOutLogin();
+                localStorage.status = this.status;
+                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "camposVazios1");
             }
             else if(!infoCheck){
-                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "usuarioNaoExiste1")
+                this.checkOutLogin();
+                localStorage.status = this.status;
+                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "usuarioNaoExiste1");
             }
             else if(infoCheck.senha === senha){ //se a senha q foi digitada na tela bate com a do banco de dados
-                this.checkLogin()
+                this.checkLogin();
+                localStorage.status = this.status;
                     //alert(`logou, você está ${this.status}`)
-                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "loginExecutado1")
+                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "loginExecutado1");
             }
             else
-                return comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "falhaLogin1")
+                 this.checkOutLogin();
+                 localStorage.status = this.status;
+                return comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "falhaLogin1");
             
         }
 }
