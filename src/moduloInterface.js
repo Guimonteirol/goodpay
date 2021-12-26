d20a972e0a7a0e373f6b9f1175f5928b29b63f22
Guimonteirol <gui.gjacobina@gmail.com>
2021-12-25 21:56:17 -0300

    Implementação do Módulo de Comunicação e associação com os modais

--- src/moduloInterface.js
+++ src/moduloInterface.js
@@ -1,31 +1,35 @@
 import userLogin from "./moduloDados.js";
+import comunicacaoModal from "./moduloComunicacaoDados.js";
 
 //CONTROLAR TODOS OS MÉTODOS DE LOGIN
 class ControlerUserLogin{
     constructor(status){
         this.status = status; //p saber se está em uso ou n
     }
-    checkLogin(){
-        this.status = true;
-    }
-
-    loginInfo(user, senha){
-        var infoCheck = userLogin.find(userLogin => userLogin.username === user ) //retorna um vetor com os objetos - procurar dentro dos dados
-        console.log(infoCheck)
-        if(!infoCheck){
-            alert("Não existe o usuário")
+        checkLogin(){
+            this.status = true;
         }
-        else{
-            if(infoCheck.senha === senha){ //se a senha q foi digitada na tela bate com a do banco de dados
+
+        loginInfo(user, senha){
+            var infoCheck = userLogin.find(userLogin => userLogin.username === user ) //retorna um vetor com os objetos - procurar dentro dos dados
+        // console.log(infoCheck) //aqui vai mostrar o vetro de dados q foi encontrtado pelo (.find)
+            if( user === null || senha === null || user === "" || senha === ""){
+                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "camposVazios1")
+            }
+            else if(!infoCheck){
+                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "usuarioNaoExiste1")
+            }
+            else if(infoCheck.senha === senha){ //se a senha q foi digitada na tela bate com a do banco de dados
                 this.checkLogin()
-                //alert(`logou, você está ${this.status}`)
+                    //alert(`logou, você está ${this.status}`)
+                return comunicacaoModal.find((comunicacaoModal)=> comunicacaoModal.idModal === "loginExecutado1")
             }
-            else{
-                alert("nn logou")
-                }
-            }
-    }
+            else
+                return comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "falhaLogin1")
+            
+        }
 }
+
 
 //estância da Classe
 const controlerUserLogin = new ControlerUserLogin();
